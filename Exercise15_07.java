7553c256bba8565ecfcb6f5bd7529ddacf39d773
manuelp1601 <mpaaredes@ivytech.edu>
2023-11-27 22:46:20 -0500

    add exercise15_07

---
+++ Exercise15_07.java
@@ -0,0 +1,33 @@
+package SDEV200;
+import java.awt.Color;
+
+public class Exercise_15_07 extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Create a Stack pane
+    StackPane pane = new StackPane();
+
+    // Create a circle and set its properties
+    Circle circle = new Circle(50);
+    circle.setFill(Color.WHITE);
+    circle.setStroke(Color.BLACK);
+    pane.getChildren().add(circle);
+
+    // Create and register the handler
+    pane.setOnMousePressed(e -> {
+      circle.setFill(Color.BLACK);
+    });
+
+    pane.setOnMouseReleased(e -> {
+      circle.setFill(Color.WHITE);
+      circle.setStroke(Color.BLACK);
+    });
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 120, 120);
+    primaryStage.setTitle("Exercise_15_07"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+
+  }
+}
